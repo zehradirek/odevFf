1394f323ee3bfe5db45c318bc0b0a830526c0d5b
whorisus <146748060+whorisus@users.noreply.github.com>
2024-04-16 03:12:56 +0300

    Dashboard Eklendi

---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss',
+})
+export class DashboardComponent {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
