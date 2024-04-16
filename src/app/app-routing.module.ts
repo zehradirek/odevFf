a05fd514db7b0fd0eb04bc65e2066ea0376e08ec
whorisus <146748060+whorisus@users.noreply.github.com>
2024-04-16 03:13:23 +0300

    Route Güncelleme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { AuthGuard } from './guards/auth.guard';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent, canActivate:[AuthGuard] },
+  { path: 'home', component: HomeComponent, canActivate:[AuthGuard] }, 
+  { path: 'dashboard', component: DashboardComponent, canActivate:[AuthGuard] }, 
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect to login page if no route matches
 ];
 
