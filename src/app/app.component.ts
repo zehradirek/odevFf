14f883c477f56a6a606953f7191189084537e9ab
whorisus <146748060+whorisus@users.noreply.github.com>
2024-04-16 03:14:57 +0300

    Flowbite kütüphanesi ekledim

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
